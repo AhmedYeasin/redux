88b1d46808b6f0a51995858c5e0a8a0d86e72eda
AhmedYeasin <ahmetchatgami@gmail.com>
2026-08-11 22:03:19 +0600

    learning Redux | First day

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button.tsx"
+
+function App() {
+
+  return (
+ <div>
+  <h2>Welcome to Vite. Learning Redux</h2>
+  <Button>Hello, Vite!</Button>
+ </div>
+  )
+}
+
+export default App
